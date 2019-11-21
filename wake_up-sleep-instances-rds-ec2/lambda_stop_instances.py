d7e5c342f8cbb7c523573164bd4288cb21f1eacd
Iván Villa Sánchez <ivan.villa.sanchez@outlook.com>
2019-11-21 14:20:42 +0100

    Update lambda_stop_instances.py

--- wake_up-sleep-instances-rds-ec2/lambda_stop_instances.py
+++ wake_up-sleep-instances-rds-ec2/lambda_stop_instances.py
@@ -1,54 +1,5 @@
 import boto3
 import json
-
-ec2 = boto3.client('ec2')
-rds = boto3.client('rds')
-ids_ec2 = []
-ids_rds = []
-
- 
-def lambda_handler(event, context):
-    list_instances_ec2()
-    try:
-        stop_instances_ec2()
-    except:
-        pass
-    list_instances_rds()
-    try:
-        stop_instances_rds()
-    except:
-        pass
-    return True
-
-def list_instances_rds():
-    dbs = []
-    reservations = rds.describe_db_instances()
-    for reservation in reservations['DBInstances']:
-        dbs.append(reservation['DBInstanceArn'])
-    for db_name in dbs:
-        response = rds.list_tags_for_resource(ResourceName=db_name)
-        for tag in response['TagList']:
-            if tag['Key'] == 'lambda_sleep_instances':
-                ids_rds.append(db_name.split("db:")[1])
-    
-def stop_instances_rds():
-    for db in ids_rds:
-        rds.stop_db_instance(DBInstanceIdentifier=db)
-
-     
-def list_instances_ec2():
-    reservations = ec2.describe_instances(
-        Filters = [
-                {'Name': 'tag-key', 'Values': ['lambda_sleep_instances']}
-            ]
-        ).get('Reservations', [])
-    instances = sum(
-            [
-                [i for i in r['Instances']]
-                for r in reservations
-            ], []
-        )import boto3
-import json
 import time
 
 ec2 = boto3.client('ec2')
@@ -100,12 +51,6 @@ def list_instances_ec2():
                 for r in reservations
             ], []
         )
-    for instance in instances:
-        ids_ec2.append(instance['InstanceId'])
- 
-def stop_instances_ec2():
-    ec2.stop_instances(InstanceIds=ids_ec2)
-
     for instance in instances:
         ids_ec2.append(instance['InstanceId'])
  
